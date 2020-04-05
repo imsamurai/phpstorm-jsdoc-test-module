41de7b5e6f2e0c110a4d478570301bb9e53e9aee
Shamray Alexander <im.samuray@gmail.com>
2020-04-05 21:20:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @module Abc */
+/**
+ * @memberOf module:Abc
+ */
+class A {
+
+    /**
+     *
+     * @param {number} x
+     * @return {Promise<number>}
+     */
+    async someAMethod(x) {
+        return x+42;
+    }
+}
+
+module.exports = A;
